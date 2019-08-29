16312066ce6e0e78689959a08643379dc4260e10
Freddy Black <freddyblackphp@gmail.com>
2019-08-29 02:57:10 -0400

    no sube

--- isa dashboard/src/app/services/isa-store/store-api.service.ts
+++ isa dashboard/src/app/services/isa-store/store-api.service.ts
@@ -10,7 +10,7 @@ export class StoreApiService {
 
   constructor(private http: HttpClient) { }
 
-  API_URI = `http://pruebas.clinicasanjuanbosco.com/db_isa_front/public/api`;
+  API_URI = `http://pruebas.clinicasanjuanbosco.com/db_isa_front/public/api`
 
   getProducts(){
     return this.http.get(`${this.API_URI}/productos`); 
